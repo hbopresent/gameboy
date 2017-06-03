2829385ce5bc6a7c1cbb9092cc93d65956e849ea
Franklin Hung <hbopresent@gmail.com>
2017-06-03 13:23:49 +0800

    update

---
+++ startgame.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.use("/app", express.static(__dirname + "/app/"));
+
+app.get("/", function(req, res) {
+    res.sendfile("main.html");
+});
+
+http.listen("56565", function() {
+  console.log("Getting gamboy started!!");
+});
